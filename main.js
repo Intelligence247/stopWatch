aeee84cca93617f268bb5c502c6dda5516886d97
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-06 10:27:52 +0100

    fixes

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 let minutes = 0
 let hrs = 0
 let seconds = 0
+let milliseconds = 0
 let minutesLeading = 0
 let hrsLeading = 0
+let secondsleading = 0
 const displayTimer = document.querySelector('.timer')
 const play = document.querySelector('.play')
 const reset = document.querySelector('.reset')
@@ -11,7 +13,12 @@ const reset = document.querySelector('.reset')
 let timerInterval = null;
 let timerStatus = 'stopped';
 const displayFunc = () => {
-    seconds++
+    milliseconds++
+    if (milliseconds / 100 === 1) {
+        seconds += 1
+        milliseconds = 0
+
+    }
     if (seconds / 60 === 1) {
         minutes += 1
         seconds = 0
@@ -20,17 +27,18 @@ const displayFunc = () => {
         minutes = 0
         hrs += 1
     }
-    hr = hrs < 10 ? hrsLeading = '0' + hrs : hrsLeading = hrs
+    hrs < 10 ? hrsLeading = '0' + hrs : hrsLeading = hrs
     minutes < 10 ? minutesLeading = `0${minutes}` : minutesLeading = minutes
-    seconds = seconds < 10 ? '0' + seconds : seconds
+    seconds < 10 ? secondsleading = '0' + seconds : secondsleading = seconds
+    milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds
 
 
-    displayTimer.innerText = `${hrsLeading}:${minutesLeading}:${seconds}`
+    displayTimer.innerText = `${hrsLeading}:${minutesLeading}:${secondsleading}:${milliseconds}`
 }
 // window.setInterval(displayFunc, 10)
 play.addEventListener('click', () => {
     if (timerStatus == 'stopped') {
-        timerInterval = window.setInterval(displayFunc, 1000);
+        timerInterval = window.setInterval(displayFunc, 10);
         const playImg = document.querySelector('#playImg')
         playImg.src = './media/pause.png'
         timerStatus = 'started'
@@ -47,5 +55,6 @@ reset.addEventListener('click', () => {
     minutes = 0
     hrs = 0
     seconds = 0
-    displayTimer.innerText = '00:00:00'
+    milliseconds = 0
+    displayTimer.innerText = '00:00:00:00'
 })
